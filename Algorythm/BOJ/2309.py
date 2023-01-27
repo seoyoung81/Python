f6b19c82df4354fbe791e1cb83c1305e0c570648
seoyoung81 <seosue0801@gmail.com>
2023-01-27 17:57:43 +0900

    2309 ing

---
+++ Algorythm/BOJ/2309.py
@@ -0,0 +1,6 @@
+height_list = []
+
+for i in range(9):
+    h = int(input())
+    height_list.append(h)
+# print(height_list) # [20, 7, 23, 19, 10, 15, 25, 8, 13]
--- Algorythm/BOJ/2605.py
+++ Algorythm/BOJ/2605.py
@@ -6,16 +6,10 @@ number_list = list(map(int, input().split()))   # 줄은 선 차례대로 뽑은
 student_list = []   # 최종적으로 줄을 선 순서를 담을 빈 리스트
 
 for i in range(len(number_list)):
-    student_list.insert((i-number_list[i]), (i+1))
-    # print((i-number_list[i]))
+    student_list.insert((i-number_list[i]), (i+1))  # (원래 index - 자기가 뽑은 인덱스 값) 자리에 (index + 1) 값을 넣자
+    # print((i-number_list[i]))   
 
-
-# # i= 0 일 때 1번 선수는 그냥 0
-# student_list.insert(0,1)
-
-# # i = 1 일 때 2번 선수가 1을 뽑았다면
-# student_list.insert(1-1, 2)    
-result = ' '.join([str(n) for n in student_list])
+result = ' '.join([str(n) for n in student_list])   # 리스트 값을 문자열로 출력하기
 print(result)
 
     
