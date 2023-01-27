be4a616c87293835752f7294486247ec5dd6fefb
고서영 <seosue0801@gmail.com>
2023-01-27 23:41:39 +0900

    2309.py add

--- Algorythm/BOJ/2309.py
+++ Algorythm/BOJ/2309.py
@@ -4,3 +4,30 @@ for i in range(9):
     h = int(input())
     height_list.append(h)
 # print(height_list) # [20, 7, 23, 19, 10, 15, 25, 8, 13]
+sub = sum(height_list) - 100    # 40
+
+# 2개 합 40 찾기 -> 36개
+done = False
+for first in height_list:
+    for second in height_list[first+1:]:
+        if first + second == sub:
+            
+            height_list.remove(first)
+            height_list.remove(second)
+            
+    if done:
+        break       
+            
+# print(height_list)
+# new_height = ''.join([str(n) for n in height_list])
+
+height_list.sort()
+
+for h in height_list:
+    print(h)
+
+
+
+
+            
+
