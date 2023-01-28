9d3c8c6f94d3f9349ba6dd7b48fc00cbbf96d506
고서영 <seosue0801@gmail.com>
2023-01-28 20:41:56 +0900

    2309 add

--- Algorythm/BOJ/2309.py
+++ Algorythm/BOJ/2309.py
@@ -7,21 +7,23 @@ for i in range(9):
 sub = sum(height_list) - 100    # 40
 
 # 2개 합 40 찾기 -> 36개
-done = False
-for first in height_list:
-    for second in height_list[first+1:]:
-        if first + second == sub:
-            
-            height_list.remove(first)
-            height_list.remove(second)
-            
-    if done:
-        break       
+first = 0   # 초기값 0 설정
+second = 0
+
+for i in range(9):
+    for j in range(i+1, 9): # first 보다 뒤에 있는 값이 필요
+        if height_list[i] + height_list[j] == sub:  # 더해서 40이면
+            first = height_list[i]      # first 에 i 할당
+            second = height_list[j]     # second 에 j 할당
+            break                       # 하나씩만 찾고 멈추기
+
+height_list.remove(first)       # first 리스트에서 삭제
+height_list.remove(second)     
             
 # print(height_list)
 # new_height = ''.join([str(n) for n in height_list])
 
-height_list.sort()
+height_list.sort()  # 오름차순으로 정렬하기
 
 for h in height_list:
     print(h)
